bf98a3939ea728e67c669d32c0020c8d8d438143
Angelo Garofalo <angelogarofalo@192.168.1.56>
2026-01-29 16:41:54 +0100

    remove brevitas warnings

--- train.py
+++ train.py
@@ -26,6 +26,9 @@ warnings.filterwarnings(
 import torch
 import torch.distributed as dist
 
+# Suppress Brevitas pkg_resources deprecation warning
+warnings.filterwarnings("ignore", category=UserWarning, module="brevitas")
+
 from datasets import get_dataset
 from models import get_model
 from utils import get_device, load_config, Trainer, set_seed
